40967b5e6d7ad61f5f807fc9e503cdc15b400733
Gregory GERARD <wako057@gmail.com>
2020-04-17 18:31:10 +0200

    feat: add basic sample

--- sample/src/main.c
+++ sample/src/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include "../include/my_putchar.h"
 
-int main() {
+
+void print_sizeof() {
 	printf("int: %ld \n", sizeof(int));
 	printf("float: %ld \n", sizeof(float));
 	printf("double: %ld \n", sizeof(double));
 	printf("char: %ld \n", sizeof(char));
+}
+
+
+int main() {
+	my_putchar('A');
 	return 0;
 }
